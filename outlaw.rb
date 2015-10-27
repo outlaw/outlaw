4dd162957cfd59bf38e2d4912a02bffb85b858c1
Jacob Evans <jacob@dekz.net>
2015-10-28 10:17:16 +1100

    Added outlaw

---
+++ outlaw.rb
@@ -0,0 +1,26 @@
+class Outlaw < Formula
+  desc ""
+  homepage ""
+  url "https://github.com/hooroo/outlaw/releases/download/0.0.1/outlaw"
+  version "0.0.1"
+  sha256 "1aa1b1483ad8ff0eb25af239e67c8c729ef74060eea5460ac1733965f4db08ba"
+
+  depends_on 'docker'
+  depends_on 'docker-machine'
+
+  def install
+  end
+
+  test do
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! It's enough to just replace
+    # "false" with the main program this formula installs, but it'd be nice if you
+    # were more thorough. Run the test with `brew test outlaw`. Options passed
+    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+    #
+    # The installed folder is not in the path, so use the entire path to any
+    # executables being tested: `system "#{bin}/program", "do", "something"`.
+    system "false"
+  end
+end

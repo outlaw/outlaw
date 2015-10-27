e0a7c1f58c7819d52c773b0e6aaa2f53682ba6a2
Jacob Evans <jacob@dekz.net>
2015-10-28 10:59:44 +1100

    Remove dependencies

--- outlaw.rb
+++ outlaw.rb
@@ -14,16 +14,11 @@ class Outlaw < Formula
     bin.install 'outlaw'
   end
 
+  def post_install
+    system "outlaw", "install"
+  end
+
   test do
-    # `test do` will create, run in and delete a temporary directory.
-    #
-    # This test will fail and we won't accept that! It's enough to just replace
-    # "false" with the main program this formula installs, but it'd be nice if you
-    # were more thorough. Run the test with `brew test outlaw`. Options passed
-    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
-    #
-    # The installed folder is not in the path, so use the entire path to any
-    # executables being tested: `system "#{bin}/program", "do", "something"`.
-    system "false"
+    system "outlaw", "-h"
   end
 end

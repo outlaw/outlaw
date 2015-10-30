58798e5f1f8f09736c5722220b69fff8070f1733
Jacob Evans <jacob@dekz.net>
2015-10-30 15:06:08 +1100

    added docker-compose

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,12 @@
-var gulp       = require('gulp'),
+var gulp    = require('gulp'),
     concat  = require('gulp-concat'),
     replace = require('gulp-replace');
-var merge = require('merge-stream');
-var fs = require('fs');
+    tar     = require('gulp-tar'),
+    gzip    = require('gulp-gzip'),
+    merge   = require('merge-stream');
 
-gulp.task('homebrew', function() {
-  var outlaw_version = fs.readFileSync('./VERSION').toString().trim();
-  return gulp.src(['outlaw.rb'])
-     .pipe(replace("OUTLAW_VERSION = ''", 'OUTLAW_VERSION = "' + outlaw_version + '"'))
-     .pipe(gulp.dest('dist'))
-});
+var fs            = require('fs'),
+    child_process = require('child_process');
 
 gulp.task('docker-osx-dev', function() {
   return gulp.src(['modules/docker-osx-dev'])
@@ -17,16 +14,38 @@ gulp.task('docker-osx-dev', function() {
      .pipe(gulp.dest('tmp'))
 });
 
-gulp.task('cowboy', ['docker-osx-dev'], function() {
+gulp.task('compose', function() {
+  return gulp.src(['modules/docker-compose.yml'])
+     .pipe(gulp.dest('tmp/dist'))
+});
+
+gulp.task('cowboy', function() {
   return gulp.src(['lib/cowboy'])
-     .pipe(concat('cowboy'))
      .pipe(gulp.dest('tmp'))
 });
 
-gulp.task('build', ['cowboy'], function() {
+gulp.task('build', ['cowboy', 'docker-osx-dev'], function() {
   return gulp.src(['tmp/docker-osx-dev', 'tmp/cowboy'])
      .pipe(concat('outlaw'))
+     .pipe(gulp.dest('tmp/dist/bin'))
+});
+
+gulp.task('package', ['build', 'compose'], function() {
+  return gulp.src('tmp/dist/**/*')
+        .pipe(tar('outlaw.tar'))
+        .pipe(gzip())
+        .pipe(gulp.dest('dist'));
+});
+
+gulp.task('homebrew', ['package'], function() {
+  var outlaw_version = fs.readFileSync('./VERSION').toString().trim();
+  var outlaw_digest = child_process.execSync('cat dist/outlaw.tar.gz | openssl dgst -sha256').toString().trim();
+
+  return gulp.src(['outlaw.rb'])
+     .pipe(replace("OUTLAW_VERSION = ''", 'OUTLAW_VERSION = "' + outlaw_version + '"'))
+     .pipe(replace("OUTLAW_DIGEST = ''", 'OUTLAW_DIGEST = "' + outlaw_digest + '"'))
      .pipe(gulp.dest('dist'))
 });
 
-gulp.task('default', ['build', 'homebrew'], function(){});
+gulp.task('dist', ['homebrew'], function() { });
+gulp.task('default', ['dist'], function(){});
--- outlaw.rb
+++ outlaw.rb
@@ -1,21 +1,27 @@
 OUTLAW_VERSION = ''
+OUTLAW_DIGEST = ''
 
 class Outlaw < Formula
-  desc ""
   homepage ""
-  url "https://github.com/hooroo/outlaw/releases/download/#{OUTLAW_VERSION}/outlaw"
+  url "https://github.com/hooroo/outlaw/releases/download/#{OUTLAW_VERSION}/outlaw.tar.gz"
+
   version OUTLAW_VERSION
-  sha256 "1aa1b1483ad8ff0eb25af239e67c8c729ef74060eea5460ac1733965f4db08ba"
+  sha256  OUTLAW_DIGEST
+
+  depends_on 'docker'         => :recommended
+  depends_on 'docker-compose' => :recommended
+  depends_on 'docker-machine' => :recommended
 
   def install
-    bin.install 'outlaw'
+    bin.install 'bin/outlaw'
+    prefix.install 'docker-compose.yml'
   end
 
   def post_install
-    system "outlaw", "install"
+    system 'outlaw', 'install'
   end
 
   test do
-    system "outlaw", "-h"
+    system 'outlaw', '-h'
   end
 end
